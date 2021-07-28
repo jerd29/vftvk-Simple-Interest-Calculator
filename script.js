96b68d25913136acadc6e79bf9f9bce43367b0f5
Jonathan Ruiz <jerd13jerd13@gmail.com>
2021-07-28 17:03:52 +0000

    Proyecto final edx web design

--- script.js
+++ script.js
@@ -2,5 +2,38 @@ function compute()
 {
     p = document.getElementById("principal").value;
     
+    var principal = document.getElementById("principal").value;
+
+    // validation of mount of input principal
+
+    if(principal == "" || principal <= 0)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    var rate = document.getElementById("rate").value;
+
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate /100;
+
+    // var years = new Date().getFullYear()+parseInt(years);
+
+    // Calculate year final year
+    var dateNow = new Date();
+    var yearNow = parseInt(dateNow.getFullYear()) + parseInt(years);
+
+    document.getElementById("result").innerHTML="If you deposit "+ "<span class='highlight'>" + principal + "</span>"+",\<br\>at an interest rate of "+ "<span class='highlight'>" + rate + "</span>"+"%\
+    <br\>You will receive an amount of "+ "<span class='highlight'>" + interest + "</span>"+",\<br\>in the year "+ "<span class='highlight'>" + yearNow + "</span>"+"\<br\>"
+
 }
         
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+
